7ba6129992f763c487f3b76eece15fffd2951afc
AkshraRajput <akshrarajput5@gmail.com>
2026-06-08 18:33:34 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -69,4 +69,7 @@ def get_interests():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
